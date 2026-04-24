acb5160f88d9f503d863d610bd89f0e315cf6a89
48570976-jpg <48570976@institutourusayhua.edu.pe>
2026-04-24 09:36:25 -0500

    nueva funcionalidad en rama3

--- app.py
+++ app.py
@@ -1,14 +1,1 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-print("MASTER - Sistema base")
-=======
-print("RAMA 1 - Registro de usuarios")
->>>>>>> rama1
-12}
-=======
-print("RAMA 2 - login de usuarios")
->>>>>>> rama2
-=======
-print("RAMA 3 : Panel administrativo")
->>>>>>> rama3
+print("RAMA 3 - Nueva funcionalidad")
